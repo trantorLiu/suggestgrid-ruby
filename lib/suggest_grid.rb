ea6cc94d07d9136b9448326955e999b98cb30c32
rof <rof@railsonfire_c8c50aa0-8d15-0133-4a77-4e70d8659446_f54c8b6bc187>
2015-12-25 09:14:48 +0000

    Update to version 0.1.5

--- lib/suggest_grid.rb
+++ lib/suggest_grid.rb
@@ -1,4 +1,4 @@
-# This file was automatically generated for SuggestGrid by APIMATIC BETA v2.0 on 12/22/2015
+# This file was automatically generated for SuggestGrid by APIMATIC BETA v2.0 on 12/24/2015
 require 'openssl'
 require 'json'
 require 'unirest'
@@ -9,6 +9,23 @@ require 'suggest_grid/api_exception.rb'
 require 'suggest_grid/configuration.rb'
 
 # Controllers
-require 'suggest_grid/controllers/api_controller.rb'
+require 'suggest_grid/controllers/action_controller.rb'
+require 'suggest_grid/controllers/bulk_controller.rb'
+require 'suggest_grid/controllers/metadata_controller.rb'
+require 'suggest_grid/controllers/recommendation_controller.rb'
+require 'suggest_grid/controllers/similarity_controller.rb'
+require 'suggest_grid/controllers/state_controller.rb'
 
 # Models
+require 'suggest_grid/models/action.rb'
+require 'suggest_grid/models/action_delete_body.rb'
+require 'suggest_grid/models/actions_model.rb'
+require 'suggest_grid/models/metadata.rb'
+require 'suggest_grid/models/users_model.rb'
+require 'suggest_grid/models/items_model.rb'
+require 'suggest_grid/models/space_request_body.rb'
+require 'suggest_grid/models/type_request_body.rb'
+require 'suggest_grid/models/recommend_users_body.rb'
+require 'suggest_grid/models/recommend_items_body.rb'
+require 'suggest_grid/models/similar_users_body.rb'
+require 'suggest_grid/models/similar_items_body.rb'
