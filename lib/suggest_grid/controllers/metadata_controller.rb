dcee4be590bd93b249cdd8bef4ef8ed45ae8adcc
rof <rof@railsonfire_1cd047d0-8d0f-0133-4a77-4e70d8659446_f6c01f0ce3d8>
2015-12-25 09:05:03 +0000

    Update to version 0.1.4

---
+++ lib/suggest_grid/controllers/metadata_controller.rb
@@ -0,0 +1,245 @@
+# This file was automatically generated for SuggestGrid by APIMATIC BETA v2.0 on 12/24/2015
+
+module SuggestGrid
+  class MetadataController
+
+    # TODO: type endpoint description here
+    # @param [Metadata] body Required parameter: TODO: type description here
+    # @param [String] space Required parameter: TODO: type description here
+    # @return MessageResponse response from the API call
+    def create_user_metadata body, space
+      # the base uri for api requests
+      query_builder = Configuration.BASE_URI.dup
+
+      # prepare query string for API call
+      query_builder << "/v1/{space}/_user"
+
+      # process optional query parameters
+      query_builder = APIHelper.append_url_with_template_parameters query_builder, {
+        "space" => space,
+      }
+
+      # validate and preprocess url
+      query_url = APIHelper.clean_url query_builder
+
+      # prepare headers
+      headers = {
+        "user-agent" => "SUGGESTGRID"
+      }
+
+      # invoke the API call request to fetch the response
+      response = Unirest.post query_url, headers:headers, parameters:body.to_json, auth:{ :user => Configuration.basic_auth_user_name, :password => Configuration.basic_auth_password }
+
+      # Error handling using HTTP status codes
+      if response.code == 400
+        raise APIException.new "Metadata is invalid.", 400, response.raw_body
+      elsif response.code == 429
+        raise APIException.new "Too many requests.", 429, response.raw_body
+      elsif response.code == 500
+        raise APIException.new "Unexpected internal error.", 500, response.raw_body
+      elsif !(response.code.between?(200,206)) # [200,206] = HTTP OK
+        raise APIException.new "HTTP Response Not OK", response.code, response.raw_body
+      end
+
+      response.body
+    end
+
+    # Delete an user metadata with the given userid.
+    # @param [String] space Required parameter: TODO: type description here
+    # @param [String] userid Required parameter: The userid to delete its metadata.
+    # @return MessageResponse response from the API call
+    def delete_an_user_metadata space, userid
+      # the base uri for api requests
+      query_builder = Configuration.BASE_URI.dup
+
+      # prepare query string for API call
+      query_builder << "/v1/{space}/_user/{userid}"
+
+      # process optional query parameters
+      query_builder = APIHelper.append_url_with_template_parameters query_builder, {
+        "space" => space,
+        "userid" => userid,
+      }
+
+      # validate and preprocess url
+      query_url = APIHelper.clean_url query_builder
+
+      # prepare headers
+      headers = {
+        "user-agent" => "SUGGESTGRID"
+      }
+
+      # invoke the API call request to fetch the response
+      response = Unirest.delete query_url, headers:headers, auth:{ :user => Configuration.basic_auth_user_name, :password => Configuration.basic_auth_password }
+
+      # Error handling using HTTP status codes
+      if response.code == 429
+        raise APIException.new "Too many requests.", 429, response.raw_body
+      elsif response.code == 500
+        raise APIException.new "Unexpected internal error.", 500, response.raw_body
+      elsif !(response.code.between?(200,206)) # [200,206] = HTTP OK
+        raise APIException.new "HTTP Response Not OK", response.code, response.raw_body
+      end
+
+      response.body
+    end
+
+    # TODO: type endpoint description here
+    # @param [String] space Required parameter: TODO: type description here
+    # @return MessageResponse response from the API call
+    def delete_all_user_metadata space
+      # the base uri for api requests
+      query_builder = Configuration.BASE_URI.dup
+
+      # prepare query string for API call
+      query_builder << "/v1/{space}/_user/_all"
+
+      # process optional query parameters
+      query_builder = APIHelper.append_url_with_template_parameters query_builder, {
+        "space" => space,
+      }
+
+      # validate and preprocess url
+      query_url = APIHelper.clean_url query_builder
+
+      # prepare headers
+      headers = {
+        "user-agent" => "SUGGESTGRID"
+      }
+
+      # invoke the API call request to fetch the response
+      response = Unirest.delete query_url, headers:headers, auth:{ :user => Configuration.basic_auth_user_name, :password => Configuration.basic_auth_password }
+
+      # Error handling using HTTP status codes
+      if response.code == 429
+        raise APIException.new "Too many requests.", 429, response.raw_body
+      elsif response.code == 500
+        raise APIException.new "Unexpected internal error.", 500, response.raw_body
+      elsif !(response.code.between?(200,206)) # [200,206] = HTTP OK
+        raise APIException.new "HTTP Response Not OK", response.code, response.raw_body
+      end
+
+      response.body
+    end
+
+    # TODO: type endpoint description here
+    # @param [Metadata] body Required parameter: TODO: type description here
+    # @param [String] space Required parameter: TODO: type description here
+    # @return MessageResponse response from the API call
+    def create_item_metadata body, space
+      # the base uri for api requests
+      query_builder = Configuration.BASE_URI.dup
+
+      # prepare query string for API call
+      query_builder << "/v1/{space}/_item"
+
+      # process optional query parameters
+      query_builder = APIHelper.append_url_with_template_parameters query_builder, {
+        "space" => space,
+      }
+
+      # validate and preprocess url
+      query_url = APIHelper.clean_url query_builder
+
+      # prepare headers
+      headers = {
+        "user-agent" => "SUGGESTGRID"
+      }
+
+      # invoke the API call request to fetch the response
+      response = Unirest.post query_url, headers:headers, parameters:body.to_json, auth:{ :user => Configuration.basic_auth_user_name, :password => Configuration.basic_auth_password }
+
+      # Error handling using HTTP status codes
+      if response.code == 400
+        raise APIException.new "Metadata is invalid.", 400, response.raw_body
+      elsif response.code == 429
+        raise APIException.new "Too many requests.", 429, response.raw_body
+      elsif response.code == 500
+        raise APIException.new "Unexpected internal error.", 500, response.raw_body
+      elsif !(response.code.between?(200,206)) # [200,206] = HTTP OK
+        raise APIException.new "HTTP Response Not OK", response.code, response.raw_body
+      end
+
+      response.body
+    end
+
+    # Delete an item metadata with the given itemid.
+    # @param [String] itemid Required parameter: The itemid to delete its metadata.
+    # @param [String] space Required parameter: TODO: type description here
+    # @return MessageResponse response from the API call
+    def delete_an_item_metadata itemid, space
+      # the base uri for api requests
+      query_builder = Configuration.BASE_URI.dup
+
+      # prepare query string for API call
+      query_builder << "/v1/{space}/_item/{itemid}"
+
+      # process optional query parameters
+      query_builder = APIHelper.append_url_with_template_parameters query_builder, {
+        "itemid" => itemid,
+        "space" => space,
+      }
+
+      # validate and preprocess url
+      query_url = APIHelper.clean_url query_builder
+
+      # prepare headers
+      headers = {
+        "user-agent" => "SUGGESTGRID"
+      }
+
+      # invoke the API call request to fetch the response
+      response = Unirest.delete query_url, headers:headers, auth:{ :user => Configuration.basic_auth_user_name, :password => Configuration.basic_auth_password }
+
+      # Error handling using HTTP status codes
+      if response.code == 429
+        raise APIException.new "Too many requests.", 429, response.raw_body
+      elsif response.code == 500
+        raise APIException.new "Unexpected internal error.", 500, response.raw_body
+      elsif !(response.code.between?(200,206)) # [200,206] = HTTP OK
+        raise APIException.new "HTTP Response Not OK", response.code, response.raw_body
+      end
+
+      response.body
+    end
+
+    # TODO: type endpoint description here
+    # @param [String] space Required parameter: TODO: type description here
+    # @return MessageResponse response from the API call
+    def delete_all_item_metadata space
+      # the base uri for api requests
+      query_builder = Configuration.BASE_URI.dup
+
+      # prepare query string for API call
+      query_builder << "/v1/{space}/_item/_all"
+
+      # process optional query parameters
+      query_builder = APIHelper.append_url_with_template_parameters query_builder, {
+        "space" => space,
+      }
+
+      # validate and preprocess url
+      query_url = APIHelper.clean_url query_builder
+
+      # prepare headers
+      headers = {
+        "user-agent" => "SUGGESTGRID"
+      }
+
+      # invoke the API call request to fetch the response
+      response = Unirest.delete query_url, headers:headers, auth:{ :user => Configuration.basic_auth_user_name, :password => Configuration.basic_auth_password }
+
+      # Error handling using HTTP status codes
+      if response.code == 429
+        raise APIException.new "Too many requests.", 429, response.raw_body
+      elsif response.code == 500
+        raise APIException.new "Unexpected internal error.", 500, response.raw_body
+      elsif !(response.code.between?(200,206)) # [200,206] = HTTP OK
+        raise APIException.new "HTTP Response Not OK", response.code, response.raw_body
+      end
+
+      response.body
+    end
+
+  end
+end
---
+++ lib/suggest_grid/models/metadata.rb
@@ -0,0 +1,29 @@
+# This file was automatically generated for SuggestGrid by APIMATIC BETA v2.0 on 12/24/2015
+
+module SuggestGrid
+  class Metadata
+
+    # TODO: Write general description for this method
+    # @return [String]
+    attr_accessor :id
+
+    
+    def method_missing (method_name)
+      puts "there's no method called '#{method_name}'"
+    end
+
+    # Creates JSON of the curent object  
+    def to_json
+      hash = self.key_map()
+      hash.to_json
+    end
+
+    # Defines the key map for json serialization  
+    def key_map
+      hash = {}
+      hash['id'] = self.id
+      hash
+    end
+
+  end
+end
