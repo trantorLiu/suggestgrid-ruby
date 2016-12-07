49d0abd4e9de64f19c38c5a3dad0ee7ffd95c2f8
Han Tuzun <mail@hantuzun.com>
2016-12-07 16:17:21 +0300

    Use master branch for proxy repos

--- bin/ci/branch/install_proxy.sh
+++ bin/ci/branch/install_proxy.sh
@@ -4,5 +4,4 @@ set -euf -o pipefail
 
 git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/suggestgrid/suggestgrid-ruby-test-proxy.git ${PROXY_PATH}
 cd ${PROXY_PATH}
-git checkout ${CIRCLE_BRANCH}
 bundle install
