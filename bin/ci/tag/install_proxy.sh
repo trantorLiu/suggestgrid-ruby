114f6c338ed69ca8e2f2a0a8d7fcf7646b29155d
Han Tuzun <mail@hantuzun.com>
2016-12-07 13:46:03 +0300

    Run proxy in circle ci

--- bin/ci/tag/install_proxy.sh
+++ bin/ci/tag/install_proxy.sh
@@ -3,4 +3,6 @@
 set -euf -o pipefail
 
 git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/suggestgrid/suggestgrid-ruby-test-proxy.git ${PROXY_PATH}
-git -C ${PROXY_PATH} checkout ${CIRCLE_TAG}
+cd ${PROXY_PATH}
+git checkout ${CIRCLE_TAG}
+bundle install
