54be2c467dd1124fa2e160a864702b157f1e0501
Han Tuzun <mail@hantuzun.com>
2016-12-07 15:33:40 +0300

    Update bin file permissions

--- bin/ci/branch/install_malik.sh
+++ bin/ci/branch/install_malik.sh
@@ -3,7 +3,7 @@
 set -euf -o pipefail
 
 git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/suggestgrid/malik.git ${MALIK_PATH}
-cd  ${MALIK_PATH}
+cd ${MALIK_PATH}
 git checkout ${CIRCLE_BRANCH}
 mkdir node_modules
 npm install
