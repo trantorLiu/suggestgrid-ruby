7eb5d29587157c41136494e59072cc806390e6e5
ubuntu <ubuntu@box215.localdomain>
2017-03-08 14:57:27 +0000

    Update error response parameter descriptions in the spec
    89128cd20a6e3789d3dee6884565386f980bc6a0

--- lib/suggest_grid/exceptions/delete_error_response_exception.rb
+++ lib/suggest_grid/exceptions/delete_error_response_exception.rb
@@ -14,15 +14,15 @@ module SuggestGrid
     # @return [String]
     attr_accessor :error_uri
 
-    # The number of records found for the delete query.
+    # The number of records found.
     # @return [Long]
     attr_accessor :found
 
-    # The number of records deleted for the delete query.
+    # The number of records deleted.
     # @return [Long]
     attr_accessor :deleted
 
-    # The number of records found but not deleted for the delete query.
+    # The number of records failed to be deleted.
     # @return [Long]
     attr_accessor :failed
 
